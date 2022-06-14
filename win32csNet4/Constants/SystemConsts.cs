957c35b851301b95e7e3746a9f96a1411a29294e
Heriberto Lugo <heribertolugo@live.com>
2022-06-14 13:51:19 -0400

    Added missing constants for system globals

--- win32csNet4/Constants/SystemConsts.cs
+++ win32csNet4/Constants/SystemConsts.cs
@@ -2,6 +2,10 @@
 {
     public static class System
     {
+        public const int MAX_DIR = 256;
+        public const int MAX_DRIVE = 3;
+        public const int MAX_EXT = 256;
+        public const int MAX_FNAME = 256;
         public const int MAX_PATH = 260;
         public const int ATTACH_PARENT_PROCESS = -1;
     }
